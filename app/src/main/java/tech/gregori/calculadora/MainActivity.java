0977aa0c5e997631ae660d95d71323b5b036a311
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2020-10-26 20:40:18 -0300

    Adicionar código para importar componentes

--- app/src/main/java/tech/gregori/calculadora/MainActivity.java
+++ app/src/main/java/tech/gregori/calculadora/MainActivity.java
@@ -3,12 +3,46 @@ package tech.gregori.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Componentes que utilizaremos em diversos métodos da classe
+    private EditText result;
+    private EditText newNumber;
+    private TextView displayOperation;
+
+    // Variáveis que contêm os operandos e as operações
+    private Double operand1 = null;
+    private Double operand2 = null;
+    private String pendingOperation = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.result);
+        newNumber = findViewById(R.id.newNumber);
+        displayOperation = findViewById(R.id.operation);
+
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button buttonDot = findViewById(R.id.buttonDot);
+
+        Button buttonEquals = findViewById(R.id.buttonEquals);
+        Button buttonDivide = findViewById(R.id.buttonDivide);
+        Button buttonMultiply = findViewById(R.id.buttonMultiply);
+        Button buttonMinus = findViewById(R.id.buttonMinus);
+        Button buttonPlus = findViewById(R.id.buttonPlus);
     }
 }
