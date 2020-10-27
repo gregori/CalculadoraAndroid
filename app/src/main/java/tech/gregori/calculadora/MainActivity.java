5500091eb97fe11af5b652597dd53048834493c1
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2020-10-26 21:42:31 -0300

    Armazenar o estado

--- app/src/main/java/tech/gregori/calculadora/MainActivity.java
+++ app/src/main/java/tech/gregori/calculadora/MainActivity.java
@@ -1,5 +1,6 @@
 package tech.gregori.calculadora;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     // Variáveis que contêm os operandos e as operações
     private Double operand1 = null;
     private String pendingOperation = "=";
+
+    private static final String STATE_PENDING_OPERATION = "PendingOperation";
+    private static final String STATE_OPERAND1 = "Operand1";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +95,23 @@ public class MainActivity extends AppCompatActivity {
         buttonMultiply.setOnClickListener(opListener);
         buttonMinus.setOnClickListener(opListener);
         buttonPlus.setOnClickListener(opListener);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString(STATE_PENDING_OPERATION, pendingOperation);
+        if (operand1 != null) {
+            outState.putDouble(STATE_OPERAND1, operand1);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION);
+        operand1 = savedInstanceState.getDouble(STATE_OPERAND1);
+        displayOperation.setText(pendingOperation);
     }
 
     private void performOperation(Double value, String operation) {
