aad243efe414bc7216f4bea4de9ca6c59f42557b
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2020-11-16 17:58:14 -0300

    Adicionar botão +/-

--- app/src/main/java/tech/gregori/calculadora/MainActivity.java
+++ app/src/main/java/tech/gregori/calculadora/MainActivity.java
@@ -95,6 +95,28 @@ public class MainActivity extends AppCompatActivity {
         buttonMultiply.setOnClickListener(opListener);
         buttonMinus.setOnClickListener(opListener);
         buttonPlus.setOnClickListener(opListener);
+
+        // Botão negação
+        Button buttonNeg = findViewById(R.id.buttonNeg);
+        buttonNeg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value = newNumber.getText().toString();
+                if (value.length() == 0) { // se campo numérico estiver vazio
+                    newNumber.setText("-");
+                } else {
+                    try {
+                        Double doubleValue = Double.valueOf(value);
+                        doubleValue *= -1;
+                        newNumber.setText(doubleValue.toString());
+                    } catch (NumberFormatException e) {
+                        // newNumber é "." ou um "-", então limpamos
+                        newNumber.setText("");
+                    }
+
+                }
+            }
+        });
     }
 
     @Override
